cd12d5f176a21680c6bb43631e4567839d4a3c7c
Adrien <adrien.podsiadly@protonmail.com>
2026-01-29 16:52:50 +0100

    add: first bits exercice

--- exercices/bits/main.c
+++ exercices/bits/main.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
+int find_LSB(uint8_t value) {
+    return (value >> 0) & 1;
+}
 int main() {
-    printf("Hello beautiful world.");
+    uint8_t user_value = 0;
+    printf("Saisir votre valeur (0 et 255): ");
+    if(scanf("%hhu",&user_value) != 1) return -1;
+    uint8_t lsb = find_LSB(user_value);
+    printf("Le LSB de %d est %d",user_value, lsb);
     return 0;
 }
