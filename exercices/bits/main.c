a70c21e9291286756523b40022b4e14e571630b6
Nyctanthos <podacc02@pm.me>
2026-02-05 16:57:52 +0100

    add: set_nth_bit function

--- exercices/bits/main.c
+++ exercices/bits/main.c
@@ -5,11 +5,19 @@ int find_LSB(uint8_t value) { return value & 1; }
 
 int find_MSB(uint8_t value) { return (value >> 7) & 1; }
 
+int find_nth_bit(uint8_t value, uint8_t nth) { return (value >> nth) & 1; }
+
+int set_nth_bit(uint8_t value, uint8_t nth) {
+  int new_value = (value >> nth) | 1;
+  printf("La nouvelle valeur est %d.\n", new_value);
+  return new_value;
+}
+
 int main() {
 
   int8_t user_choice = 0;
 
-  printf("MSB[0]-LSB[1]: ");
+  printf("MSB[0]-LSB[1]-NTH[2]-NTH[3]: ");
   if (scanf("%hhu", &user_choice) != 1)
     return 1;
 
@@ -35,6 +43,17 @@ int main() {
   case 1:
     res = find_LSB(user_value);
     printf("Le LSB de %u est %u:i\n", user_value, res);
+    break;
+  case 2:
+    printf("NTH bit  \n");
+    uint8_t nth;
+    if (scanf("%hhu", &nth) != 1)
+      return -1;
+    res = find_nth_bit(user_value, nth);
+    printf("Le %d bit de %d est %d\n", nth, user_value, res);
+    break;
+  case 3:
+    set_nth_bit(user_value, nth);
     break;
   default:
     printf("Argument Invalide");
