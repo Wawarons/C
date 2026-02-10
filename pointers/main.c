ac540a7497799825a6268ea6b2bcb5f9d8da3b16
Nyctanthos <podacc02@pm.me>
2026-02-10 10:40:19 +0100

    update: refactor

--- pointers/main.c
+++ pointers/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
+int sum(int a, int b) {
+    int *ptr_1, *ptr_2;
+    ptr_1 = &a;
+    ptr_2 = &b;
+
+    return *ptr_1 + *ptr_2;
+}
+
 int main() {
+
+    //Sum
     int a = 12;
     int b = 14;
-    int *p = &a;
-    int *q = &b;
-    int r = *p + *q;
 
-    printf("%d + %d = %d", a, b, r);
+    printf("%d + %d = %d", a, b, sum(a,b));
+
     return 0;
 }
